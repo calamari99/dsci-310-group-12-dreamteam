e165280148c09ace91a224fd68565a508274113b
calamari99 <ajienoch@gmail.com>
2023-04-07 18:44:54 -0700

    First attempt clean script & make file

--- R/02_clean_data.R
+++ R/02_clean_data.R
@@ -1,15 +1,22 @@
-## Milestone 2:
-## Cleans data by selecting only necessary columns:
-## 
-##
-## First selects all necessary coluumns, removes NA data
-## and splits into paid vs unpaid posts before giving a summary of the 
-## splits
-##
-# @ param: facebook = dataset
+# author: Enoch Cheung
+# date: 2023-04-07
 
+doc <- "
+Cleans data by selecting only necessary columns: First selects all necessary coluumns, removes NA data,
+and splits into paid vs unpaid posts before giving a summary of the splits
 
+Usage: 
+  R/01_read_data_from_URL.R --url=<url> --out_dir=<out_dir>
+Options:
+--input_dir=<input>     raw data file
+--out_dir=<out_dir>   clean data dir
+" 
+#libraries
 library(dplyr)
+
+opt <- docopt(doc)
+
+
 clean_data <- function(facebook) {
   facebook_clean <<- na.omit(dplyr::select(facebook, Type, comment,
                                   like, share, Total_Interactions,
@@ -28,7 +35,8 @@ clean_data <- function(facebook) {
   paid_summary <<- facebook_clean_paid %>% group_by(Type) %>% 
     summarise(paid = n())
   
-  print("Table 1. Facebook data summary")
   Reduce(dplyr::full_join, list(unpaid_summary, paid_summary)) %>%
     suppressMessages()
+
+  write_csv(facebook_clean_paid, file.path(out_dir, file_name))
 }
