f7b97fdd1eeb00d585d700f8002e5de8e639c59d
calamari99 <ajienoch@gmail.com>
2023-04-07 17:31:20 -0700

    fixed maxfile syntax

--- R/01_read_data_from_URL.R
+++ R/01_read_data_from_URL.R
@@ -14,32 +14,30 @@ Options:
 library(readr)
 library(docopt)
 options(repr.matrix.max.rows = 8)
-suppressPackageStartupMessages(library(docopt))
-suppressPackageStartupMessages(library(caTools))
-suppressPackageStartupMessages(library(dplyr))
-suppressPackageStartupMessages(library(readr))
-suppressPackageStartupMessages(library(ggplot2))
-suppressPackageStartupMessages(library(tidyr))
-suppressPackageStartupMessages(library(grid))
-suppressPackageStartupMessages(library(gridExtra))
-suppressPackageStartupMessages(library(kknn))
-suppressPackageStartupMessages(library(repr))
-suppressPackageStartupMessages(library(cowplot))
-suppressPackageStartupMessages(library(tidyverse))
-suppressPackageStartupMessages(library(tidymodels))
+library(docopt)
+library(caTools)
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(tidyr)
+library(grid)
+library(gridExtra)
+library(kknn)
+library(repr)
+library(cowplot)
+library(tidyverse)
+library(tidymodels)
 
 opt <- docopt(doc)
 
 main <- function(url,out_dir) {
-  url="https://raw.githubusercontent.com/calamari99/Facebook-Post-Predictor/main/data/dataset_Facebook.csv"
-  out_dir="../data/facebook_data.csv"
   facebook <<- read_csv2(url) 
   cols <- c("Type", "Category", "Post Month", "Paid", "Post Weekday", "Post Hour")  #set cols to change to categorical %>%
   facebook[cols] <<- lapply(facebook[cols], as.factor)
   name_temp <<- facebook
   name_temp_vec <<- gsub(" ", "_", colnames(facebook))
   colnames(facebook) <<- name_temp_vec
-  write_csv(facebook, "../data/facebook_data.csv")
+  write_csv(facebook, out_dir)
 
 #  Save file location
 
