86ba320563649804d29beafa7ae4254896335dbc
Enoch Chenug <ajienoch@gmail.com>
2023-04-10 20:37:10 -0700

    Testing step 6, knn model, need make file to run

---
+++ R/06_knn.R
@@ -0,0 +1,48 @@
+# author: Enoch Cheung
+# date: 2023-04-08
+
+doc <- "
+Take valset and trainset variables and start building model. (i) Pre-tuned (ii) 
+
+Usage: 
+  R/06_knn--input_dir=<input_dir> --out_dir=<out_dir>
+Options:
+--input_dir=<input_dir>     train/val data
+--out_dir=<out_dir>         data dir
+" 
+
+main <- function( 
+
+    train_set_unpaid <- readRDS(data/train_set_unpaid.rds)
+    val_set_unpaid <- readRDS(data/val_set_unpaid.rds)
+
+    knn_spec <- nearest_neighbor(weight_func = "rectangular", neighbors = 3) %>%
+      set_engine("kknn") %>%
+      set_mode("classification")
+
+    unpaid_recipe <- recipe(Type ~ 
+                            Lifetime_Post_Total_Reach + Total_Interactions,
+                            data = train_set_unpaid) %>%
+                        step_scale(all_predictors()) %>%
+                        step_center(all_predictors())
+
+    unpaid_fit <- workflow() %>%
+        add_recipe(unpaid_recipe) %>%
+        add_model(knn_spec) %>%
+        fit(data = train_set_unpaid)
+
+    unpaid_val_predicted <- predict(unpaid_fit, val_set_unpaid) %>%
+        bind_cols(val_set_unpaid)
+
+    unpaid_prediction_accuracy <- unpaid_val_predicted %>%
+        metrics(truth = Type, estimate = .pred_class)
+
+    unpaid_val_predicted %>%
+        conf_mat(truth = Type, estimate = .pred_class)
+    
+    print("06_run")
+)
+
+
+# call main
+main(opt[["--input_dir"]], opt[["--out_dir"]])
